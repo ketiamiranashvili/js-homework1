4f3e888aefcf8a0fc9676303862e9b686cd01525
ketiamiranashvili <qetiamiranashvili670@gmail.com>
2023-12-22 13:32:59 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+//first exercise
+const x = 100;
+
+if( x < 50){
+    console.log ("naklebia 50-ze")
+}else if( x>20){
+    console.log("metia 20-ze")
+}else {
+    console.log ("false")
+}
+
+//secound exercise
+
+const userName = "mariam";
+
+
+//if-else
+if(userName){
+    console.log("true")
+}else{
+    console.log ("false")
+}
+
+const girlName ="mariam";
+
+//thirdexercise
+
+//switch-case
+switch (userName){
+    case 'mariam' :
+        console.log("true")
+        break;
+        case "else" :
+            console.log("false")
+
+}
+
+//fourth exercise
+let info = prompt ('enter a number');
+console.log('info');
+
+let number = 10;
+
+if(number %2 == 0){
+    console.log("kentia")
+}
+if (number %2 == 1){
+    console.log("luwia")
+}
